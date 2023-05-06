09b177bd99569d0b3d9bd83dcc3079c329a09490
Tarun Vangani <reachtarunhere@gmail.com>
2023-05-07 01:54:01 +0800

    Just the main function stub

--- src/JuLox.jl
+++ src/JuLox.jl
@@ -1,5 +1,14 @@
 module JuLox
 
-# Write your package code here.
+function main(args)
+    if length(args) > 1
+        println("Usage: JuLox [script]")
+        exit(64)
+    elseif length(args) == 1
+        run_file(args[1])
+    else
+        run_prompt()
+    end
+end
 
 end
