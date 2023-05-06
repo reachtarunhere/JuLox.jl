41511549c30da91fe9b48d4dcf62f0d43910d662
Tarun Vangani <reachtarunhere@gmail.com>
2023-05-07 02:45:14 +0800

    Add prompt loop and dummy function for executing the file

--- src/JuLox.jl
+++ src/JuLox.jl
@@ -1,5 +1,7 @@
 module JuLox
 
+run_file(path) = run(read(path))
+
 function main(args)
     if length(args) > 1
         println("Usage: JuLox [script]")
@@ -10,5 +12,18 @@ function main(args)
         run_prompt()
     end
 end
+
+function run_prompt()
+    while true
+        print("JuLox> ")
+        flush(stdout)
+        line = readline()
+        isempty(line) && eof(stdin) && break
+        println(line)
+    end
+end
+
+# eof is blocking so it is not possible to use it in a while loop
+# condition. TODO: sill buggy behaviour with empty line and then typing
 
 end
